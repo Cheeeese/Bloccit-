ad06c14fdf0a0fdadfa48aeab025958f059db2e3
Matt V <matt.verghese@gmail.com>
2014-06-05 15:11:16 +0000

    Fixing merge issues

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-#CarrierWave.configure do |config|
-#  config.fog_credentials = {
-#    provider:               'AWS',
-#    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
-#    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
-#  }
-
-#  config.fog_directory = ENV['AWS_BUCKET']
-#  config.fog_public = true
-#end
-=======
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',
@@ -20,4 +8,3 @@ CarrierWave.configure do |config|
   config.fog_directory = ENV['AWS_BUCKET']
   config.fog_public = true
 end
->>>>>>> uploading-images
